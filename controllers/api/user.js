b516c6e4e5a6387f224aaac27eeed307e38559d4
Christian Groselle <christian.groselle@gmail.com>
2022-12-13 20:15:52 -0500

    updated new user route to use await, and started adding user put route

--- controllers/api/user.js
+++ controllers/api/user.js
@@ -3,31 +3,35 @@ const User = require('../../models/User');
 
 router.get('/:id', async (req, res) => {
     try {
-    // Search the database for a user with an id that matches params
-    const userData = await User.findByPk(req.params.id);
-    console.log(userData)
-    // We use .get({ plain: true }) on the object to serialize it so that it only includes the data that we need. 
-    const user = userData.get({ plain: true });
-    res.render('user', user);
+      // Search the database for a user with an id that matches params
+      const userData = await User.findByPk(req.params.id);
+      console.log(userData)
+      // We use .get({ plain: true }) on the object to serialize it so that it only includes the data that we need. 
+      const user = userData.get({ plain: true });
+      res.render('user', user);
     } catch (err) {
         res.status(500).json(err);
     }
   });
 
 router.post('/', async (req, res) => {
-    User.create({
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        email: req.body.email,
-        password: req.body.password
-      })
-        .then((newUser) => {
-          // Send the newly created row as a JSON object
-          res.json(newUser);
-        })
-        .catch((err) => {
-          res.json(err);
-        });
+  try {
+    const dbUserData = await User.create({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      password: req.body.password
+      });
+      // Set up sessions with a 'loggedIn' variable set to `true`
+    req.session.save(() => {
+      req.session.loggedIn = true;
+
+      res.status(200).json(dbUserData);
+    });
+  } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+  }
 });
 
   module.exports = router;
