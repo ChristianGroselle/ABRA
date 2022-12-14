52ce42df69decf83a4162bd62abcce72b60cfc58
Nuo <codypham5@gmail.com>
2022-12-13 21:39:12 -0500

    Working login and logout

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,8 +1,33 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const { User } = require("../models");
 
-router.get('/', async (req, res) => {
-  // Send the rendered Handlebars.js template back as the response
-  res.render('homepage');
+router.get("/", async (req, res) => {
+  try {
+    // const userData = await User.findAll({
+    //   attributes: { exclude: ["password"] },
+    //   order: [["name", "ASC"]],
+    // });
+
+    // const users = userData.map((project) => project.get({ plain: true }));
+
+    res.render("homepage", {
+      // users,
+      // Pass the logged in flag to the template
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/login", (req, res) => {
+  // If a session exists, redirect the request to the homepage
+  if (req.session.logged_in) {
+    res.redirect("/");
+    return;
+  }
+
+  res.render("login");
 });
 
 module.exports = router;
